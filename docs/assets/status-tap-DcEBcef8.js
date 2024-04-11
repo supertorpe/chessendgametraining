deea6d73007236a1873e8b77ce6c0ac07f73c711
supertorpe <supertorpe@hotmail.com>
2024-04-11 18:37:40 +0200

    publish updates

---
+++ docs/assets/status-tap-DcEBcef8.js
@@ -0,0 +1,4 @@
+import{a0 as r,a1 as i,a2 as c,a3 as d,a4 as l}from"./vendor-BCPMU-lZ.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
+//# sourceMappingURL=status-tap-DcEBcef8.js.map
