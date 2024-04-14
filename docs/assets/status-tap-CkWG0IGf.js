6ef63b47566d0be8d88c1d097ed1dbcc324ed80e
supertorpe <supertorpe@hotmail.com>
2024-04-14 14:10:31 +0200

    publish updates

---
+++ docs/assets/status-tap-CkWG0IGf.js
@@ -0,0 +1,4 @@
+import{a2 as r,a3 as i,a4 as c,a5 as d,a6 as l}from"./vendor-CuDTzKpS.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
+//# sourceMappingURL=status-tap-CkWG0IGf.js.map
---
+++ docs/assets/stop-CMatVGto.js
@@ -0,0 +1,2 @@
+const a="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20class='ionicon'%20viewBox='0%200%20512%20512'%3e%3cpath%20d='M392%20432H120a40%2040%200%2001-40-40V120a40%2040%200%200140-40h272a40%2040%200%200140%2040v272a40%2040%200%2001-40%2040z'/%3e%3c/svg%3e";export{a as default};
+//# sourceMappingURL=stop-CMatVGto.js.map
