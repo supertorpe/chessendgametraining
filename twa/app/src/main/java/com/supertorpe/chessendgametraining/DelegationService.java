cc1e8a1d730bbd0e8b43a45dcb104afb9bd5c63e
supertorpe <supertorpe@hotmail.com>
2024-05-05 19:45:01 +0200

    TWA

---
+++ twa/app/src/main/java/com/supertorpe/chessendgametraining/DelegationService.java
@@ -0,0 +1,14 @@
+package com.supertorpe.chessendgametraining;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
