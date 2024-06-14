108a69131f424bf29325f1855d2152f766fe44ff
supertorpe <supertorpe@hotmail.com>
2024-06-14 09:02:06 +0200

    about page: show database version

--- code/src/controllers/about.ts
+++ code/src/controllers/about.ts
@@ -3,11 +3,13 @@ import { version } from '../../package.json';
 import { BaseController } from './controller';
 import { aboutJson } from '../static';
 import { IonModal } from '@ionic/core/components/ion-modal';
+import { endgameDatabaseService } from '../services';
 
 class AboutController extends BaseController {
     onEnter(_$routeParams?: any): void {
         Alpine.data('about', () => ({
             version: version,
+            databaseVersion: endgameDatabaseService.endgameDatabase.version,
             items: aboutJson.items.map(item => { return {url: item.url, text: window.AlpineI18n.t(item.i18n) } }),
             close() {
                 (document.querySelector('ion-modal') as IonModal).dismiss();
